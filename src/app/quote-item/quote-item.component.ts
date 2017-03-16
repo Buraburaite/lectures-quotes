b0817c89a7c4325f1e77394e8ad6b3b2f1dc059c
Javier <jvrcueto@outlook.com>
2017-03-16 10:52:20 -0400

    intial

---
+++ src/app/quote-item/quote-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-quote-item',
+  templateUrl: './quote-item.component.html',
+  styleUrls: ['./quote-item.component.css']
+})
+export class QuoteItemComponent implements OnInit {
+
+  @Input() quote: any;
+  @Input() author: any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
