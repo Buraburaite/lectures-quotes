75d116189f40edfa23852ee05665a9f5a5bc8804
Javier <jvrcueto@outlook.com>
2017-03-17 10:08:53 -0400

    working on services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,27 +2,52 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { QuoteItemComponent } from './quote-item/quote-item.component';
 import { QuoteListComponent } from './quote-list/quote-list.component';
+import { MyHomeComponent } from './my-home/my-home.component';
+import { MyAboutComponent } from './my-about/my-about.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { CountersComponent } from './counters/counters.component';
+import { MyCounterComponent } from './my-counter/my-counter.component';
 
 const routes: Routes = [
-  // localhost:4200/quotes
-  { path: 'quotes', component: QuoteListComponent }
+  // http://localhost:4200/quotes
+  { path: 'quotes', component: QuoteListComponent },
+  // http://localhost:4200/home
+  { path: 'home', component: MyHomeComponent },
+  // http://localhost:4200/about
+  { path: 'about', component: MyAboutComponent },
+
+  { path: 'contacts', component: ContactListComponent },
+
+  { path: 'contact/:id', component: ContactComponent },
+
+  { path: 'counters', component: CountersComponent },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' }
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
     QuoteItemComponent,
-    QuoteListComponent
+    QuoteListComponent,
+    MyHomeComponent,
+    MyAboutComponent,
+    ContactListComponent,
+    ContactComponent,
+    CountersComponent,
+    MyCounterComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
